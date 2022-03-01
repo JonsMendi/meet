1137f8b32dced2031f43c5a6a1576ae4ed071f6e
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-03-01 19:40:50 -0100

    updated with new test to pass the number of events state

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme/build';
+import { shallow, mount } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
 import CitySearch from '../CitySearch';
@@ -80,5 +80,13 @@ describe('<App /> integration', () => {
         expect(AppWrapper.state('events')).toEqual(allEvents);
         AppWrapper.unmount();
     });
+
+    test("passes the number of events state", () => {
+        const AppWrapper = mount(<App />);
+        const AppNumberOfEventsState = AppWrapper.state("numberOfEvents");
+        expect(AppNumberOfEventsState).not.toEqual(undefined);
+        expect(AppWrapper.find(EventList).props().numberOfEvents).toEqual(32);
+        AppWrapper.unmount();
+      });
 
 });
