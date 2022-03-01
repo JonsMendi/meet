af42628b6bc19413512490df06ced52f825534a7
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-03-01 19:45:04 -0100

    Updated
    If applied, this commit will update the updateEvents() with new state of numberOfEvents.
    This new state was as well added to <EventList >

--- src/App.js
+++ src/App.js
@@ -1,56 +1,62 @@
 import React, { Component } from 'react';
 import './App.css';
+import './nprogress.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { extractLocations, getEvents } from './api';
-import './nprogress.css';
+
 
 class App extends Component {
   state = {
     events: [],
-    locations: []
+    locations: [],
+    numberOfEvents: 32
   }
-
-  /*Under, updateEvent is created to change the state in 'events' when a user clicks in a 'suggested' city.
-  This method will then be passed to CitySearch component as props. 
-  Finally this method will be used inside the 'handleItemClicked', in other words, when the user 'clicks' in the city*/
-  updateEvents = (location) => {
-    getEvents()
-    .then((events) => {
-      const locationEvents = (location === 'all')
-      ?
-      events :
-      events.filter((event) => event.location === location);
-      this.setState({
-        events: locationEvents
-      });
-    });
-  }
-
 
   /*Under the componentDidMount loads events when the app loads. 
   We'll use componentDidMount in App.js to make the API call and save the initial data to state */
-  componentDidMount() {
+   componentDidMount() {
     this.mounted = true;
-    getEvents()
-    .then((events) => {
+    getEvents().then((events) => {
       if (this.mounted) {
-        this.setState({ events, locations: extractLocations(events) });
+        this.setState({
+          events: events.slice(0, this.state.numberOfEvents),
+          locations: extractLocations(events),
+        });
       }
-    })
+    });
   }
 
   componentWillUnmount() {
     this.mounted = false;
   }
 
+
+  /*Under, updateEvent is created to change the state in 'events' when a user clicks in a 'suggested' city.
+  This method will then be passed to CitySearch component as props. 
+  Finally this method will be used inside the 'handleItemClicked', in other words, when the user 'clicks' in the city*/
+  updateEvents = (location, eventCount) => {
+    getEvents().then((events) => {
+      const locationEvents =
+        location === "all"
+          ? events
+          : events.filter((event) => event.location === location);
+      if (this.mounted) {
+        this.setState({
+          events: locationEvents.slice(0, this.state.numberOfEvents),
+          currentLocation: location,
+        });
+      }
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents}/>
-        <EventList events={this.state.events} />
-        <NumberOfEvents />
+        <EventList events={this.state.events} numberOfEvents={this.state.numberOfEvents}/>
+        <NumberOfEvents  />
       </div>
     );
   }
