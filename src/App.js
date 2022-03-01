3d98017e94f7042a611235769f8e81005083d887
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-03-01 14:25:24 -0100

    added new updatedEvents functions, componentDidMount() and componentWillUnmount()
    If applied, this commit will have added  a updatedEvents functions, componentDidMount() and componentWillUnmount()

--- src/App.js
+++ src/App.js
@@ -3,13 +3,52 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
+import { extractLocations, getEvents } from './api';
 
 class App extends Component {
+  state = {
+    events: [],
+    locations: []
+  }
+
+  /*Under, updateEvent is created to change the state in 'events' when a user clicks in a 'suggested' city.
+  This method will then be passed to CitySearch component as props. 
+  Finally this method will be used inside the 'handleItemClicked', in other words, when the user 'clicks' in the city*/
+  updateEvents = (location) => {
+    getEvents()
+    .then((events) => {
+      const locationEvents = (location === 'all')
+      ?
+      events :
+      events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents
+      });
+    });
+  }
+
+
+  /*Under the componentDidMount loads events when the app loads. 
+  We'll use componentDidMount in App.js to make the API call and save the initial data to state */
+  componentDidMount() {
+    this.mounted = true;
+    getEvents()
+    .then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    })
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
   render() {
     return (
       <div className="App">
-        <CitySearch />
-        <EventList />
+        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents}/>
+        <EventList events={this.state.events} />
         <NumberOfEvents />
       </div>
     );
