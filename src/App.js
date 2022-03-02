671f501ccb766ec0c3756a4cb3148d832d7cea2a
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-03-02 16:20:47 -0100

    Fixed NumberOfEvents filter.
    If applied, this commit will have fixed the NumberOfEvents filtered according the the input.
    For this was added a new state "currentLocation" that accepts the user "location" in updateEvents method.
    Using slice() was added the state of "numberOfEvents" to "events" state that contains the "event.location".
    Was created another method "updateNumberOfEvents" that set the state of the input(eventCount) value and update the "updateEvents" state adding the "currentLocation" and "numberOfEvents" own state (inputed number).
    Afterwards this methods have been passed as props in NumberOfEvents.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ class App extends Component {
   state = {
     events: [],
     locations: [],
-    numberOfEvents: 32
+    numberOfEvents: 32,
+    currentLocation: 'all'
   }
 
   /*Under the componentDidMount loads events when the app loads. 
@@ -36,27 +37,36 @@ class App extends Component {
   /*Under, updateEvent is created to change the state in 'events' when a user clicks in a 'suggested' city.
   This method will then be passed to CitySearch component as props. 
   Finally this method will be used inside the 'handleItemClicked', in other words, when the user 'clicks' in the city*/
-  updateEvents = (location, eventCount) => {
+  updateEvents = (location) => {
     getEvents().then((events) => {
-      const locationEvents =
-        location === "all"
-          ? events
-          : events.filter((event) => event.location === location);
-      if (this.mounted) {
+      const locationEvents = (location === "all") 
+      ? events
+      : events.filter((event) => event.location === location);
+      if( this.mounted){
         this.setState({
-          events: locationEvents.slice(0, this.state.numberOfEvents),
-          currentLocation: location,
+        events: locationEvents.slice(0, this.state.numberOfEvents),//the events that will be display are added by the slice() that joins the other(numberOfEvents) state.
+        currentLocation: location
         });
-      }
+
+      }   
     });
   };
+
+  updateNumberOfEvents = (eventCount) => {
+    const updateNumber = eventCount.target.value;
+    this.setState({ numberOfEvents: updateNumber });
+    this.updateEvents(this.state.currentLocation, this.state.numberOfEvents);
+  };
+
+  
+  
 
   render() {
     return (
       <div className="App">
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents}/>
-        <EventList events={this.state.events} numberOfEvents={this.state.numberOfEvents}/>
-        <NumberOfEvents  />
+        <EventList events={this.state.events} />
+        <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateNumberOfEvents={this.updateNumberOfEvents}/>
       </div>
     );
   }
