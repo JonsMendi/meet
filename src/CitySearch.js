2cf3133adc85a1cedeeaf43d0f6c2ab59d0cc053
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-03-09 18:56:36 -0100

    added InfoAlert as condition to handleInputChanged
    If applied, this commit will have added InfoAlert as condition to handleInputChanged to be show when User type city that doesn't exist in the App.

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component{
     state = {
@@ -9,19 +10,35 @@ class CitySearch extends Component{
 
     handleInputChanged = (event) => {
         const value = event.target.value;
+        this.setState({
+            showSuggestions: true
+        });
         const suggestions = this.props.locations.filter((location) => {
             return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
         });
-        this.setState({ 
-            query: value,
-            suggestions,
-         });
+        /*After the list of suggestion is returned (above), the If condition (under) 
+        will check if the list contains the same input name, if not, the InfoText will be show. 
+        If there is a suggestion, the InfoText will have an empty string.*/
+        if (suggestions.length === 0) {
+            this.setState({ 
+                query: value,
+                showSuggestions: false,
+                infoText: 'We can not find the city you are looking for. Please try another city'
+            });    
+        } else {
+            return this.setState({
+                query: value,
+                suggestions,
+                infoText: '',
+            });
+        } 
     };
 
     handleItemClicked = (suggestion) => {
         this.setState({
           query: suggestion,
-          showSuggestions: false
+          showSuggestions: false,
+          infoText: '',
         });
         /*'suggestion' being passed to handleItemClicked() is the variable that’s being
         passed to the map loop function’s callback.This is a different way to write a
@@ -32,6 +49,7 @@ class CitySearch extends Component{
     render () {
         return (
             <div className="CitySearch">
+                <label className="city-search_label">City Search</label>
                 <input 
                     type="text" 
                     className="city" 
@@ -41,13 +59,16 @@ class CitySearch extends Component{
                 <ul className="suggestions" 
                     style={this.state.showSuggestions ? {} : { display: 'none' }}>
                     {this.state.suggestions.map((suggestion) => (
-                        <li key={suggestion} 
-                        onClick={() => this.handleItemClicked(suggestion)}>{suggestion}</li>
+                        <li 
+                        key={suggestion} 
+                        onClick={() => this.handleItemClicked(suggestion)}
+                        >{suggestion}</li>
                     ))}
                     <li onClick={() => this.handleItemClicked("all")}>
                         <b>See all cities</b>
                     </li>
                 </ul>
+                <InfoAlert text={this.state.infoText} />
             </div>
         )
     }
