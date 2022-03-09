81ddd9191681305247374794a6420a859b4c55bc
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-03-09 18:54:39 -0100

    updated with new ErrorAlert by props.
    If applied, this commit will have added a new ErrorAlert by props defined in App.js

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
     render () {
         return (
-            <div>
+            <div className="numberOfEvents">
+                <label>Number of Events</label>
                 <input 
                     type="number"
                     className="number-events" 
                     value={this.props.numberOfEvents} 
                     onChange={(e) => this.props.updateNumberOfEvents(e)}/>
+                <ErrorAlert text={this.props.errorAlert} />    
             </div>
         )
     }
