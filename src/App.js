b3fff12651c0e03805c5e903d15b440704a3d70a
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-03-15 13:05:59 -0100

    updated with WelcomeScreen
    If applied, this commit will have added the WelcomeScreen to the file as initial page.

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './nprogress.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import { extractLocations, getEvents } from './api';
 import meetyourapp from './images/meetyourapp.png';
 import { WarningAlert } from './Alert';
-
+import WelcomeScreen from './WelcomeScreen';
+import { extractLocations, getEvents, checkToken, getAccessToken } from './api';
 
 
 class App extends Component {
@@ -16,20 +16,35 @@ class App extends Component {
     locations: [],
     numberOfEvents: 32,
     currentLocation: 'all',
+    showWelcomeScreen: undefined
   }
 
   /*Under the componentDidMount loads events when the app loads. 
-  We'll use componentDidMount in App.js to make the API call and save the initial data to state */
-   componentDidMount() {
+  We'll use componentDidMount in App.js to make the API call and save the initial data to state.
+  What is happening under?
+  - First, we're trying to get the token from localStorage (it goes
+  to localStorage when the getToken() function in “src/api.js” is called). Then, we’re trying to verify it
+  using another function from “src/api.js”—checkToken()—hence why you needed to export it from
+  there. If there’s an error in the object returned by checkToken(), the variable isTokenValid will be
+  assigned with the value false; otherwise, it will be true. */
+   async componentDidMount() {
     this.mounted = true;
-    getEvents().then((events) => {
-      if (this.mounted) {
-        this.setState({
-          events: events.slice(0, this.state.numberOfEvents),
-          locations: extractLocations(events),
-        });
-      }
-    });
+    const accessToken = localStorage.getItem('access_token');
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({
+            events: events.slice(0, this.state.numberOfEvents),
+            locations: extractLocations(events),
+          });
+        }
+      });
+    }
   }
 
   componentWillUnmount() {
@@ -74,16 +89,32 @@ class App extends Component {
   };
 
   render() {
+    /*The state 'showWelcomeScreen' will be used as a flag to determine when to render the welcome screen
+    as follows: true will mean “show the welcome screen,” false will mean “hide it to show the
+    other components,” and undefined will be used to render an empty div until the state gets
+    either true or false: */
+    if (this.state.showWelcomeScreen === undefined) 
+      return <div className='App' />
+
     return (
       <div className="App">
-        { !navigator.onLine ? (<WarningAlert text='Sorry, you are in offline mode!' />) : (<WarningAlert text=' ' />)}
         <img className='main-image' src={meetyourapp} alt="meet-your-apa_image" />
         <NumberOfEvents 
           numberOfEvents={this.state.numberOfEvents} 
           updateNumberOfEvents={this.updateNumberOfEvents} 
           errorAlert ={this.state.errorAlert}/>
-        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents}/>
-        <EventList events={this.state.events} />
+        <CitySearch 
+          locations={this.state.locations} 
+          updateEvents={this.updateEvents}/>
+        {/* --- */}
+        {/*Under the WarningAlert is displayed if the user is offline. 'onLine' is using NProgress package */}
+        { !navigator.onLine ? (<WarningAlert text='Sorry, you are in offline mode!' />) : (<WarningAlert text=' ' />)}
+        {/* --- */}
+        <EventList 
+          events={this.state.events} />
+        <WelcomeScreen 
+          showWelcomeScreen={this.state.showWelcomeScreen} 
+          getAccessToken={() => { getAccessToken() }} />
       </div>
     );
   }
